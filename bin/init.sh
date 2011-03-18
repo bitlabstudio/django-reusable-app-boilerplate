998c22d8b38578ca4ff0711d3d801044900ea4c6
Martin Brochhaus <mbrochh@gmail.com>
2011-03-18 19:14:05 +0800

    fixed bug in sed command

--- bin/init.sh
+++ bin/init.sh
@@ -18,14 +18,7 @@ sed -i "s/the_keywords/$KEYWORDS/g" setup.py
 sed -i "s/the_author/$AUTHOR/g" setup.py
 sed -i "s/the_email/$AUTHOREMAIL/g" setup.py
 sed -i "s/the_url/$URL/g" setup.py
-find . -type f -exec sed -i "s/appname/$APPNAME/g" + {}
-#sed -i "s/appname/$APPNAME/g" setup.py
-#sed -i "s/appname/$APPNAME/g" appname/admin.py
-#sed -i "s/appname/$APPNAME/g" appname/urls.py
-#sed -i "s/appname/$APPNAME/g" appname/views.py
-#sed -i "s/appname/$APPNAME/g" appname/tests/runtests.py
-#sed -i "s/appname/$APPNAME/g" appname/tests/tests.py
-#sed -i "s/appname/$APPNAME/g" appname/tests/urls.py
+find . -type f -exec sed -i "s/appname/$APPNAME/g" {} +
 
 echo "Renaming folders..."
 mv appname/templates/appname/ appname/templates/$APPNAME
